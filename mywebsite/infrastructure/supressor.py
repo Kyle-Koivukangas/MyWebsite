7a0e8cb6582625ec713c1a1a51d6c98bdc12e01d
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-07-22 10:20:02 -0700

    Fixed JS links & added login form
    JS links werent working or being hashed, I corrected that and also added a login form to the 'login' dropdown in the navbar. I also fixed typos in blog/index.pt and suppressor function.

--- mywebsite/infrastructure/supressor.py
+++ mywebsite/infrastructure/supressor.py
@@ -2,5 +2,5 @@ import pyramid_handlers
 
 class suppress(pyramid_handlers.action):
     def __init__(self, _, **kw):
-        kw['request_method'] = ["RanDoM_BuLLsHiT_ThAt_iS_noT_aN_HTTP_VeRb_oR_ReQuEsT"]
+        kw['request_method'] = ["RanDoM_BuLLsHiT_ThAt_iS_noT_aN_HTTp_VeRb_oR_ReQuEsT"]
         super().__init__(**kw)
