267eeaf513297dd8e5fb6620bef876aebe72f0d0
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-07-22 19:24:34 -0700

    Bug fix, String not Int
     Account.id value should be a String, not Int.

--- mywebsite/data/account.py
+++ mywebsite/data/account.py
@@ -2,6 +2,7 @@ import datetime
 import uuid
 import sqlalchemy.orm
 from sqlalchemy import Column, String, Boolean, DateTime
+import sqlalchemy
 
 from mywebsite.data.modelbase import SqlAlchemyBase
 
@@ -9,9 +10,9 @@ from mywebsite.data.modelbase import SqlAlchemyBase
 class Account(SqlAlchemyBase):
     __tablename__ = 'Account'
 
-    id = Column(sqlalchemy.Integer, primary_key=True, default=lambda: str(uuid.uuid4()).replace('-', ''))
+    id = Column(sqlalchemy.String, primary_key=True, default=lambda: str(uuid.uuid4()).replace('-', ''))
     email = Column(sqlalchemy.String, index=True, unique=True, nullable=False)
     password = Column(sqlalchemy.String, index=True, unique=True, nullable=False)
-    created = Column(DateTime, default=datetime.datetime.now)
+    created = Column(DateTime, index=True, default=datetime.datetime.now)
     email_confirmed = Column(Boolean, nullable=False, default=False)
     is_super_user = Column(Boolean, nullable=False, default=False)
