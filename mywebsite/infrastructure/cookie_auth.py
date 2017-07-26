1950cdf054dcc224b109f088c269cf6030c649e6
kyle-koivukangas <kyle.koivukangas@gmail.com>
2017-07-25 19:49:44 -0700

    Added salt to hash so cookie is tamper-proof.

--- mywebsite/infrastructure/cookie_auth.py
+++ mywebsite/infrastructure/cookie_auth.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 auth_cookie_name = "kkoivu_user"
 
 def set_auth(request, user_id):
-    hash_val = hashlib.sha512(user_id.encode('utf-8')).hexdigest()
+    hash_val = __hash_text(user_id)
     val = "{}:{}".format(user_id, hash_val)
 
     request.response.set_cookie(auth_cookie_name, val, max_age=timedelta(days=30))
@@ -24,9 +24,13 @@ def get_user_id_from_cookie(request):
 
     user_id = parts [0]
     hash_val = parts[1]
-    hash_val_check = hashlib.sha512(user_id.encode('utf-8')).hexdigest()
+    hash_val_check = __hash_text(user_id)
     if hash_val != hash_val_check:
         print("Hash mismatch, invalid cookie")
         return None
 
     return user_id
+
+def __hash_text(text):
+    text = "my_saltshake_brings" + text + "all_the_boys_to_the_yard"
+    return hashlib.sha512(text.encode('utf-8')).hexdigest()
