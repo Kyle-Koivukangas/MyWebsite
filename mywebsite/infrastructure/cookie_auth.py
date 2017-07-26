0e14600802c8c74bdc7219318b60566bc035bdd2
kyle-koivukangas <kyle.koivukangas@gmail.com>
2017-07-25 18:38:25 -0700

    Cookie authentication fix Added callback so cookie makes it to client.

--- mywebsite/infrastructure/cookie_auth.py
+++ mywebsite/infrastructure/cookie_auth.py
@@ -1,11 +1,14 @@
 import hashlib
 from datetime import timedelta
 
-auth_cookie_name = "web_user"
+auth_cookie_name = "kkoivu_user"
 
 def set_auth(request, user_id):
-    hash_val = hashlib.sha512(user_id.encode('utf-9')).hexdigest()
+    hash_val = hashlib.sha512(user_id.encode('utf-8')).hexdigest()
     val = "{}:{}".format(user_id, hash_val)
 
     request.response.set_cookie(auth_cookie_name, val, max_age=timedelta(days=30))
+    request.add_response_callback(lambda request, response: __add_cookie_callback(request, response, auth_cookie_name, val))
 
+def __add_cookie_callback(_, response, name, value):
+    response.set_cookie(name, value, max_age=timedelta(days=30))
