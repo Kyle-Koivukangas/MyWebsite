1f2717c6276f00cddfa81d2e83966b03228e41a6
kyle-koivukangas <kyle.koivukangas@gmail.com>
2017-07-25 19:25:56 -0700

    Added cookie login state save. added get_user_id_from_cookie function for saving the login state of the user via cookies.

--- mywebsite/infrastructure/cookie_auth.py
+++ mywebsite/infrastructure/cookie_auth.py
@@ -12,3 +12,21 @@ def set_auth(request, user_id):
 
 def __add_cookie_callback(_, response, name, value):
     response.set_cookie(name, value, max_age=timedelta(days=30))
+
+def get_user_id_from_cookie(request):
+    if auth_cookie_name not in request.cookies:
+        return None
+
+    val = request.cookies[auth_cookie_name]
+    parts = val.split(':')
+    if len(parts) != 2:
+        return None
+
+    user_id = parts [0]
+    hash_val = parts[1]
+    hash_val_check = hashlib.sha512(user_id.encode('utf-8')).hexdigest()
+    if hash_val != hash_val_check:
+        print("Hash mismatch, invalid cookie")
+        return None
+
+    return user_id
