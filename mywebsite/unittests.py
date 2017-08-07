752598c6ceaa05c33db07dc021dfe5b25a2c47b6
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-08-07 16:17:18 -0700

    Need to do unit tests.. Haven't been doing unit tests, Need to implement ssome form of testing.

---
+++ mywebsite/unittests.py
@@ -0,0 +1,55 @@
+import unittest
+from pyramid import testing
+
+#from mywebsite.services.account_service import AccountService
+
+class ViewTests(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp()
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_my_view(self):
+        from .views import my_view
+        request = testing.DummyRequest()
+        info = my_view(request)
+        self.assertEqual(info['project'], 'mywebsite')
+
+class FunctionalTests(unittest.TestCase):
+
+    def setUp(self):
+        from mywebsite import main
+        app = main({})
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+
+    def test_root(self):
+        res = self.testapp.get('/', status=200)
+        self.assertTrue(b'Pyramid' in res.body)
+
+    def test_account_service(self):
+        # self.assertEqual(AccountService.get_accounts()[hi@hi.com], 
+        print(AccountService.get_accounts().values())
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+# class TestStringMethods(unittest.TestCase):
+
+#     def test_upper(self):
+#         self.assertEqual('foo'.upper(), 'FOO')
+
+#     def test_isupper(self):
+#         self.assertTrue('FOO'.isupper())
+#         self.assertFalse('Foo'.isupper())
+
+#     def test_split(self):
+#         s = 'hello world'
+#         self.assertEqual(s.split(), ['hello', 'world'])
+#         # check that s.split fails when the separator is not a string
+#         with self.assertRaises(TypeError):
+#             s.split(2)
