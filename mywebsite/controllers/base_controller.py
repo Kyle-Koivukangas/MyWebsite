cee4f1d48c5ab218b35616aeef37c6a6941ea168
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-08-06 21:49:07 -0700

    Changed main layout to grids Change base controller to use the gris layout instead of bootstrap layout

--- mywebsite/controllers/base_controller.py
+++ mywebsite/controllers/base_controller.py
@@ -15,7 +15,7 @@ class BaseController:
         self.build_cache_id = static_cache.build_cache_id
 
         #grab layout and make available to all controllers
-        layout_render = pyramid.renderers.get_renderer('mywebsite:templates/shared/_bootstrap_layout.pt')
+        layout_render = pyramid.renderers.get_renderer('mywebsite:templates/shared/_layout_grids.pt')
         impl = layout_render.implementation()
         self.layout = impl.macros['layout']
 
