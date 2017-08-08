8fef50a24b517f12853d7992eb1f1f5a2a126ae7
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-08-07 21:28:11 -0700

    Added javascript function to highlight active navbar link. Copy/pasted function wasn't working, had to spend some time to figure it out and write my own.

--- mywebsite/static/js/jscript.js
+++ mywebsite/static/js/jscript.js
@@ -1,8 +1,21 @@
 
-$(function() { 
-    $('nav-item').on('click','nav-item', function ( e ) {
-        e.preventDefault();
-        $(this).parents('nav-item').find('active').removeClass('active').end().end().addClass('active');
-        $(activeTab).show();
-    });
+console.log("js loaded");
+
+$(document).ready(function() { 
+// this will get the full URL at the address bar and slice off the last element or two, depending on how many there are
+var path = window.location.href.split("/").splice(3,4);
+
+//checks incase path is clean (no sub directories, fresh homepage) or it has multple parts
+if (path == '') {
+    path = 'index';
+}
+if (path.length > 1) {
+    path = path.join("/");
+}
+console.log(path);
+
+//target the navbar link that's the same as the path and add 'active' class to it to highlight it.
+var target = $('nav a[href="/'+path+'"]');
+$(target.addClass('active'));
+
 });
