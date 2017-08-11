9497859e138a630ecffdd834c918c39001fde583
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-08-10 22:22:24 -0700

    testing

--- mywebsite/static/js/test.js
+++ mywebsite/static/js/test.js
@@ -1,13 +1,29 @@
+// following this tutorial, will finish tomorrow.. https://www.youtube.com/watch?v=rJesac0_Ftw
+
+var animalContainer = document.getElementById("animal-info");
 var btn = document.getElementById('btn');
 
 btn.addEventListener('click', function() {
+
+  console.log("click registered");
+
   var ourRequest = new XMLHttpRequest();
-  
-  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
+  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
   ourRequest.onload = function() {
     var our_data = JSON.parse(ourRequest.responseText);
-    console.log(our_data[0]);
+    console.log(our_data);
+    renderHTML(our_data);
   };
   ourRequest.send();
-  
+
 });
+
+function renderHTML(data) {
+  var HTML_string = "";
+
+  for (i = 0; i < data.length; i++) {
+    HTML_string += "<p>" + data[i].name + "is a" + data[i].species + ".</p>";
+  };
+  animalContainer.insertAdjacentHTML('beforeend', HTML_string);
+
+};
