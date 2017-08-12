cd2e9d66fcfe942e04aa681f61bc20433edcd11e
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-08-11 20:05:00 -0700

    done testing handlebars. finished handlebars tutorial (https://www.youtube.com/watch?v=wSNa5b1mS5Y).

--- mywebsite/static/js/test.js
+++ mywebsite/static/js/test.js
@@ -1,10 +1,13 @@
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/pets-data.json');
+
 ourRequest.onload = function() {
   if (ourRequest.status >= 200 && ourRequest.status < 400) {
     // This is where we'll do something with the retrieved data
-    var data = JSON.parse(ourRequest.responseText);
-    console.log(data);
+    var petsData = JSON.parse(ourRequest.responseText);
+
+    createHTML(petsData);
+
   } else {
     console.log("We connected to the server, but it returned an error.");
   }
@@ -14,4 +17,24 @@ ourRequest.onerror = function() {
   console.log("Connection error");
 };
 
-ourRequest.send();
+ourRequest.send();  
+
+Handlebars.registerHelper("calculateAge", function(birthYear) {
+  var age = new Date().getFullYear() - birthYear;
+  if (age < 1) {
+    return "Less than a year old."
+  } else{ 
+    return age + " years old."
+  };
+});
+
+
+function createHTML (data) {
+  console.log(data);
+  var rawTemplate = document.getElementById("petsTemplate").innerHTML;
+  var compiledTemplate = Handlebars.compile(rawTemplate);
+  var ourGeneratedHTML = compiledTemplate(data);
+
+  var petsContainer = document.getElementById("pets-container");
+  petsContainer.innerHTML = ourGeneratedHTML;
+}
