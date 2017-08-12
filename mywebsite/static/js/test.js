43d9bb45b97e6a4b466835de94d012fdc9040990
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-08-11 18:28:13 -0700

    Groundwork for handlebars tutorial.

--- mywebsite/static/js/test.js
+++ mywebsite/static/js/test.js
@@ -1,57 +1,17 @@
-// following this tutorial, will finish tomorrow.. https://www.youtube.com/watch?v=rJesac0_Ftw
-
-var animalContainer = document.getElementById("animal-info");
-var btn = document.getElementById('btn');
-
-btn.addEventListener('click', function() {
-
-  console.log("click registered");
-
-  var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-  ourRequest.onload = function() {
-    if (ourRequest.status >= 200 && ourRequest.status <400) {
-      var our_data = JSON.parse(ourRequest.responseText);
-      console.log(our_data);
-      renderHTML(our_data);
-    } else {
-      console.log("Connected to server, but server returned an error: " + ourRequest.status);
-    };
-  };
-  
-  ourRequest.onerror = function() {
-    console.log("connection error");
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/pets-data.json');
+ourRequest.onload = function() {
+  if (ourRequest.status >= 200 && ourRequest.status < 400) {
+    // This is where we'll do something with the retrieved data
+    var data = JSON.parse(ourRequest.responseText);
+    console.log(data);
+  } else {
+    console.log("We connected to the server, but it returned an error.");
   }
-  ourRequest.send();
-
-});
-
-function renderHTML(data) {
-  var HTML_string = "";
-
-  for (i = 0; i < data.length; i++) {
-    HTML_string += "<p>" + data[i].name + " is a " + data[i].species + ', ' + data[i].name + "'s favourite foods are: ";
-    
-    for (ii = 0; ii < data[i].foods.likes.length; ii++) {
-      if (ii == 0) {
-        HTML_string += data[i].foods.likes[ii]; 
-      } else {
-        HTML_string += ", " + data[i].foods.likes[ii];
-      }
-           
-    }
-    HTML_string += '. ' + data[i].name + " doesn't like ";
-    for (ii = 0; ii < data[i].foods.dislikes.length; ii++) {
-      if (ii == 0) {
-        HTML_string += data[i].foods.dislikes[ii]; 
-      } else {
-        HTML_string += " or " + data[i].foods.dislikes[ii];
-      }
-           
-    } 
-
-    HTML_string += ".</p>";
-  };
-  animalContainer.insertAdjacentHTML('beforeend', HTML_string);
-
 };
+
+ourRequest.onerror = function() {
+  console.log("Connection error");
+};
+
+ourRequest.send();
