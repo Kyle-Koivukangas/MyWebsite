ae57c8a0463a80c9b057f0a3b885e3c86eecdb4f
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-08-14 17:08:42 -0700

    added projects JSON controller. updated bower components, the ones that were in there before I did the git ignore are being updated.. there are lots

--- mywebsite/controllers/projects_controller.py
+++ mywebsite/controllers/projects_controller.py
@@ -12,6 +12,27 @@ class ProjectsController(BaseController):
         return {'value': 'PROJECTS/INDEX'}
 
     @pyramid_handlers.action(renderer='json')
+    def projectsJSON(self):
+        """serves JSON file for the projects page, this file will contain the HTML for each project which will be inserted in to
+        the page via javascript"""
+        response_data = {}
+
+        # the following will need to be tweaked, I need to first write a script that pulls the HTML from the project templates 
+        # and packs it in to a properly formatted JSON file rather than doing that in the controller.
+        with open('mywebsite/templates/projects.json', 'r') as f:
+            projects = [line.rstrip('\n') for line in f]
+            try:
+                response_data['result'] = 'success'
+                response_data['message'] = ''.join(projects)
+            except:
+                response_data['result'] = 'oops!'
+                response_data['message'] = 'error reading file'
+                
+        return json.dumps(response_data, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
+
+
+    @pyramid_handlers.action(renderer='json')
     def pokerbot(self):
         response_data = {}
 
@@ -26,3 +47,4 @@ class ProjectsController(BaseController):
                 
         return json.dumps(response_data, default=lambda o: o.__dict__, 
             sort_keys=True, indent=4)
+
